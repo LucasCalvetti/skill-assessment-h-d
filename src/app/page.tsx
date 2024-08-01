b8bbe4521a3c85096ef0f2175ccc6aa5c2efe81b
Lucas Calvetti <l.calvetti.dev@gmail.com>
2024-07-31 21:58:26 -0300

    refactor: modularize Home components into small ones (uploadForm)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,10 @@
-"use client";
-import React from "react";
-import { Button } from "@/components/ui/button";
-
+import { UploadForm } from "@/components/forms/uploadForm";
+import React, { useState } from "react";
 export default function Home() {
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file && file.size > 5 * 1024 * 1024) {
-      //TODO: error management here
-      return console.log("File too big");
-    }
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const data = Object.fromEntries(formData.entries());
-    console.log(data);
-  };
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-3xl font-bold mb-5">Upload your file</h1>
-      <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
-        <input type="file" onChange={handleInputChange} />
-        <Button type="submit">Hola</Button>
-      </form>
+      <UploadForm />
     </main>
   );
 }
