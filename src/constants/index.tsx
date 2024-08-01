b340d358dcf5332af222860fab0dad3109b6c693
Lucas Calvetti <l.calvetti.dev@gmail.com>
2024-07-31 23:47:05 -0300

    Add api urls to constants

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -12,3 +12,11 @@ export const UPLOADING_BUTTON_TEXT = {
   [APP_STATUS.READY_UPLOAD]: "Upload file",
   [APP_STATUS.UPLOADING]: "Uploading...",
 } as const;
+
+//API url vercel
+export const API_URL = "https://www.postb.in/1722475618549-5473569461610";
+
+//Fake APIs
+export const BEGIN_UPLOAD_API = "https://example.com/begin-upload";
+export const SUCCESS_UPLOAD_API = "https://example.com/success-upload";
+export const FAILURE_UPLOAD_API = "https://example.com/failure-upload";
