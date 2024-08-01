d53218e4732bfcd2ee2d2e79342175ee15c68ddc
Lucas Calvetti <l.calvetti.dev@gmail.com>
2024-07-31 23:48:27 -0300

    Enhance form component with error handling, state management, and API integration

--- src/components/forms/uploadForm.tsx
+++ src/components/forms/uploadForm.tsx
@@ -1,8 +1,10 @@
 "use client";
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { APP_STATUS, UPLOADING_BUTTON_TEXT } from "@/constants";
+import { APP_STATUS, UPLOADING_BUTTON_TEXT, API_URL, BEGIN_UPLOAD_API, SUCCESS_UPLOAD_API, FAILURE_UPLOAD_API } from "@/constants";
 import { type AppStatusType } from "@/types";
+import { uploadFile } from "@/api";
+import { toast } from "sonner";
 
 export function UploadForm() {
   const [file, setFile] = useState<File | null>(null);
@@ -22,19 +24,34 @@ export function UploadForm() {
     setAppStatus(APP_STATUS.READY_UPLOAD);
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    //TODO: send file to API
+    if (appStatus != APP_STATUS.READY_UPLOAD || !file) {
+      //TODO: error management here
+      return;
+    }
+    setAppStatus(APP_STATUS.UPLOADING);
+    const [err, result] = await uploadFile(file, API_URL);
+    if (err) {
+      setAppStatus(APP_STATUS.ERROR);
+      toast.error(err.message);
+      return;
+    }
+    setAppStatus(APP_STATUS.READY_USAGE);
+    toast.success("File uploaded successfully");
   };
 
-  const showButtonFlag = appStatus == APP_STATUS.READY_UPLOAD || appStatus == APP_STATUS.READY_USAGE;
+  const showButtonFlag = appStatus === APP_STATUS.READY_UPLOAD || appStatus === APP_STATUS.UPLOADING;
   return (
     <form className="flex flex-col" onSubmit={handleSubmit}>
       <label className="block mb-3">
         <input disabled={appStatus == APP_STATUS.UPLOADING} type="file" onChange={handleInputChange} />
       </label>
-      {showButtonFlag && <Button variant={"secondary"}>{UPLOADING_BUTTON_TEXT[appStatus]}</Button>}
+      {showButtonFlag && (
+        <Button disabled={appStatus == APP_STATUS.UPLOADING} variant={"secondary"}>
+          {UPLOADING_BUTTON_TEXT[appStatus]}
+        </Button>
+      )}
     </form>
   );
 }
