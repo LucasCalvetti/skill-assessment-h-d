5a93aa675d126d4496cc191c03dc96c14b002c78
Lucas Calvetti <l.calvetti.dev@gmail.com>
2024-08-01 06:30:53 -0300

    Add Context provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
-
+import { BlobListProvider } from "@/context/blobContext";
 import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
@@ -23,10 +23,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-        {children}
-        <Toaster />
-      </body>
+      <BlobListProvider>
+        <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+          {children}
+          <Toaster />
+        </body>
+      </BlobListProvider>
     </html>
   );
 }
